869b383b4a248e03e02b7ffc3714e8c98584c845
濵戸悠圭 <>
2020-07-09 04:44:45 +0900

    コード削減

--- public/js/script.js
+++ public/js/script.js
@@ -1,58 +1,31 @@
 $(function(){
-  $('#submit-btn').click(function(){
-    var familyName = $('input[name="familyName"]').val();
-    var firstName = $('input[name="firstName"]').val();
-    var email = $('input[name="e-mail"]').val();
-    var sex = $('input[name="sex"]:checked').attr('id');
-    var age = $('[name="age"] option:selected').val();
-    var note = $('textarea[name="note"]').val();
 
-    console.log( familyName );
-    console.log( firstName );
-    console.log( email );
-    console.log( sex );
-    console.log( age );
-    console.log( note );
-  })
+  $('#submit-btn').click(function() {
+    var formData = $('form').serializeArray();
+    $.each(formData,function(index,val){
+      console.log(val.value);
+    })
+  });
 
   $('#temp-btn').click(function(){
     $('input[name="familyName"]').val('gizumo');
     $('input[name="firstName"]').val('華子');
     $('input[name="e-mail"]').val('gizumo@gmail.com');
     $('#female').prop('checked',true);
-    $('[name="age"] option[value="20~29"]').prop('selected',true);
+    $('[name="age"]').val('20~29');
     $('textarea[name="note"]').val('hondaに勤めている');
-  })
+  });
 
   $('#disabled-btn').click(function(){
-    var prop = $('#submit-btn').prop('disabled');
-    if (prop) {
-      $('input[name="familyName"]').prop('disabled', false);
-      $('input[name="firstName"]').prop('disabled', false);
-      $('input[name="e-mail"]').prop('disabled', false);
-      $('input[name="sex"]').prop('disabled', false);
-      $('[name="age"]').prop('disabled', false);
-      $('textarea[name="note"]').prop('disabled', false);
-      $('#submit-btn').prop('disabled', false);
-      $('#clear-btn').prop('disabled', false);
-      $('#temp-btn').prop('disabled', false);
-      $('#disabled-btn').removeClass('btn-info');
-      $('#disabled-btn').addClass('btn-danger');
-      $('#disabled-btn').text('操作不能');
+    if ($('#submit-btn').prop('disabled')) {
+      $('input, select, textarea, button').not(this).prop('disabled', false);
+      $(this).toggleClass('btn-danger btn-info');
+      $(this).text('操作不能');
     } else {
-      $('input[name="familyName"]').prop('disabled', true);
-      $('input[name="firstName"]').prop('disabled', true);
-      $('input[name="e-mail"]').prop('disabled', true);
-      $('input[name="sex"]').prop('disabled', true);
-      $('[name="age"]').prop('disabled', true);
-      $('textarea[name="note"]').prop('disabled', true);
-      $('#submit-btn').prop('disabled', true);
-      $('#clear-btn').prop('disabled', true);
-      $('#temp-btn').prop('disabled', true);
-      $('#disabled-btn').removeClass('btn-danger');
-      $('#disabled-btn').addClass('btn-info');
-      $('#disabled-btn').text('解除');
+      $('input, select, textarea, button').not(this).prop('disabled', true);
+      $(this).toggleClass('btn-danger btn-info');
+      $(this).text('解除');
     }
-  })
+  });
 
   });
